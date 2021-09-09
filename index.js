6bc8f98688b1a22c382b5fa1dbe38d8be69ae011
drox13 <dario.baron@uptc.edu.co>
2021-09-08 20:23:21 -0500

    se esta corriendo la base de datos apenas se levanta el servicio (prueba)

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const app = express()
+const shell = require('shelljs')
+const mysql = require('mysql')
+
+
+//const execSync = require("exec-sync");
 const port = 3000
+const PATH = process.cwd();
+
+// let connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'todoapp'
+// });
+
+execSync(PATH + `/database/startdb.sh`);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+//shell.exec-sync(PATH + `/database/startdb.sh`);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
