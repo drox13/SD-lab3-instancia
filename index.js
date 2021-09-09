1bc1a08ff40f251b4a97d9784104aef34f9a8c56
AndresCrg <36751170+AndresCrg@users.noreply.github.com>
2021-09-09 12:17:55 -0500

    Se crean los scripts para obtener ram y cpu del sistema

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require('express')
-const app = express()
-const shell = require('shelljs')
-const mysql = require('mysql')
-
+const express = require('express');
+const app = express();
+const shell = require('shelljs');
+const mysql = require('mysql');
 
 //const execSync = require("exec-sync");
-const port = 3000
+const port = 3000;
 const PATH = process.cwd();
 
 // let connection = mysql.createConnection({
@@ -18,11 +17,31 @@ const PATH = process.cwd();
 //execSync(PATH + `/database/startdb.sh`);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+	res.send('Hello World!');
+});
+
+app.get('/resource/cpu', (req, res) => {
+	const cpuData = shell.exec(PATH + '/cpuUsage.ps1');
+	if (cpuData != null) {
+		console.log(cpuData);
+		res.send(cpuData);
+	} else {
+		console.log('error');
+	}
+});
+
+app.get('/resource/ram', (req, res) => {
+	const ramFreeData = shell.exec(PATH + '/ramAvalible.sh');
+	console.log(ramFreeData);
+	if (cpuData != null) {
+		res.send(ramFreeData);
+	} else {
+		console.log('error');
+	}
+});
 
 shell.exec(PATH + `/database/startdb.sh`);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+	console.log(`Example app listening at http://localhost:${port}`);
+});
